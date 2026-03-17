d721f325efd9c6bdc17ca51836c1ea708b8d603e
redaelhend <reda.elhend@gmail.com>
2026-03-17 17:04:11 +0000

    first commit

---
+++ password_chek.py
@@ -0,0 +1,13 @@
+while True:
+    code=input("chose a password :")
+    if len(code)>=8:
+        break
+    else:
+        print("lente of the passeword need to be more than 8 caractairs")
+
+# chek if he have caractair and numebre and sumbole
+# chek if he use a patern 
+# if note give hime a segtion with ia
+# give hime the shose to aprov it or refusit 
+# if he refeuset give hime a nother shose 
+# if he aprovit give hime the shose to exite 
