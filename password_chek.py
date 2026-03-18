99aa9252a165b76294d979f0488fbd7808d364ea
redaelhend <reda.elhend@gmail.com>
2026-03-18 22:37:56 +0000

    i add part tha he chose if he like the pasword or not if not i give hime anather one

--- password_chek.py
+++ password_chek.py
@@ -1,11 +1,13 @@
-# this just a code you will need to instal rockyou.txt to run it with this
+# to run this code you will need :
+# install rovkyou filie ( filie tha have same populair pasword)
+#add a API key in the variable client
 
 import string
 import time
 from groq import Groq
 
 client = Groq(api_key="api key of grop ;]")
-
+# fonction for ask the ai fo a pasword
 def get_ai_suggestion(password):
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
@@ -29,7 +31,7 @@ while True:
 # chek if he use a patern 
 start=time.time()
 etat=False
-
+# chek evry cod in the rockyou if he matche the code xhosing by the user
 with open("rockyou.txt","r", encoding="latin-1")as f:
     for i in f :
         if code == i.strip() :
@@ -37,6 +39,7 @@ with open("rockyou.txt","r", encoding="latin-1")as f:
             break
 
 end=time.time()
+# print how mush time need to see if the pasword in the rockyou
 print(f"Time taken: {end - start:.2f} seconds")
 
 # chek if he have caractair and numebre and sumbole
@@ -63,13 +66,31 @@ if etat==False:
 else:
     print("it is a coment pasword \n Suggested stronger password:", get_ai_suggestion(code))
 
+# control what will hapend depend of his segestion
+
+
+
+
 while True:
     if chose == "y" or chose=="Y":
         print("Suggested stronger password:", get_ai_suggestion(code))
-        break
+        fell=input("did you like it ? Y/N :")
+
+        while True:
+            if fell == "y" or fell=="Y":
+                break
+            elif fell == "n" or fell=="N": 
+                print("Suggested stronger password:", get_ai_suggestion(code))
+                fell=input("did you like it ? Y/N :")
+                continue
+            else:
+                print("chose a valide respond")
+                fell=input("did you like it ? Y/N :")
+
     elif chose == "n" or chose=="N":
-        print("havee a nice day")
         break
     else:
         print("you chose a invalide choise ")
         chose=input("would you like a segetion for a better one? y/n :")
+
+print("have a nice day <3")
